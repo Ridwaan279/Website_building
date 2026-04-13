8d068616fec6dfac9defaa29c66b64e0f34a9b42
Ridwaan Joomun <132223969+Ridwaan279@users.noreply.github.com>
2026-04-13 13:52:07 +0100

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Website_building/',
   plugins: [
     react(),
     tailwindcss(),
